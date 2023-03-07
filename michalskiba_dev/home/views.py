8f15471b6f3ffe3c993bfb35aa274f431572123c
dev-michal-skiba <dev.michal.skiba@gmail.com>
2023-03-07 20:27:35 +0100

    Added navbar

--- michalskiba_dev/home/views.py
+++ michalskiba_dev/home/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("michalskiba.dev home page WIP")
+    return render(request, 'home.html')
