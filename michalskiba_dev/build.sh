ec72578dabe36b6238b404fce7d6e409aa7d22ba
dev-michal-skiba <dev.michal.skiba@gmail.com>
2023-05-24 19:43:55 +0200

    Updated static files to work on production

--- michalskiba_dev/build.sh
+++ michalskiba_dev/build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-
-set -o errexit  # exit on error
+set -o errexit
 
 pip install -r requirements.txt
 
-#python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 python manage.py migrate
--- michalskiba_dev/michalskiba_dev/settings.py
+++ michalskiba_dev/michalskiba_dev/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -124,9 +125,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 BASE_STATIC_PATH = BASE_DIR / "static"
-
+if DEBUG is False:
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIRS = [BASE_STATIC_PATH]
 
 # Default primary key field type
