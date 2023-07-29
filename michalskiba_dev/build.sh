a959577a7d2ab2fb9844b9771bafc31c3fb2726a
dev-michal-skiba <dev.michal.skiba@gmail.com>
2023-07-29 11:49:29 +0200

    Fixed build script

--- michalskiba_dev/build.sh
+++ michalskiba_dev/build.sh
@@ -2,10 +2,9 @@
 set -o errexit
 
 pip install -r requirements.txt
-mkdir static/blog/posts
 
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py migrate --database=web_parameter_tampering
-python manage.py detect_new_raw_files.py
-python manage.py detect_new_post_files.py
+python manage.py detect_new_raw_files
+python manage.py detect_new_post_files
