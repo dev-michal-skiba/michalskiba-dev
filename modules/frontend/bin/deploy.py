1757d0e352e9df8259610783ef2819c6e588cbf1
dev-michal-skiba <dev.michal.skiba@gmail.com>
2024-03-06 17:31:01 +0100

    Added support for content-type

--- modules/frontend/bin/deploy.py
+++ modules/frontend/bin/deploy.py
@@ -26,11 +26,41 @@ def clean_remote_destination_directory(s3_client):
     print("Remote destination directory cleaned")
 
 
+def _get_content_type(filepath):
+    _, extension = filepath.split(".")
+    if extension == "js":
+        return "application/javascript"
+    if extension == "html":
+        return "text/html"
+    if extension == "txt":
+        return "text/plain"
+    if extension == "json":
+        return "application/json"
+    if extension == "ico":
+        return "image/x-icon"
+    if extension == "svg":
+        return "image/svg+xml"
+    if extension == "css":
+        return "text/css"
+    if extension in ["jpg", "jpeg"]:
+        return "image/jpeg"
+    if extension == "png":
+        return "image/png"
+    if extension == "webp":
+        return "image/webp"
+    if extension == "map":
+        return "binary/octet-stream"
+    if extension == "xml":
+        return "text/xml"
+    return "application/octet-stream"
+
+
 def upload(s3_client):
     print("Uploading ...")
     for root, _, filenames in os.walk(LOCAL_SOURCE_DIRECTORY):
         for file_name in filenames:
             local_source_file_path = os.path.join(root, file_name)
+            content_type = _get_content_type(local_source_file_path)
             remote_destination_file_path = (
                 REMOTE_DESTINATION_DIRECTORY
                 + os.path.relpath(
@@ -38,7 +68,10 @@ def upload(s3_client):
                 )
             )
             s3_client.upload_file(
-                local_source_file_path, BUCKET_NAME, remote_destination_file_path
+                local_source_file_path,
+                BUCKET_NAME,
+                remote_destination_file_path,
+                ExtraArgs={"ContentType": content_type},
             )
     print("Uploaded")
 
