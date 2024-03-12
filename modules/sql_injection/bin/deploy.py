7adc0a786caf0b529577e2d3af207fe6e74d22df
dev-michal-skiba <dev.michal.skiba@gmail.com>
2024-03-12 22:56:49 +0100

    Created deployment script

---
+++ modules/sql_injection/bin/deploy.py
@@ -0,0 +1,36 @@
+import os
+
+import boto3
+
+LOCAL_SOURCE_DIRECTORY = "src/"
+LOCAL_LAMBDA_PACKAGE_FILENAME = "lambda_package.zip"
+LAMBDA_FUNCTION_NAME = "michalskiba-dev-sql-injection"
+
+
+def build_module() -> None:
+    print("Building module ...")
+    os.remove(LOCAL_LAMBDA_PACKAGE_FILENAME)
+    os.system(
+        f"cd {LOCAL_SOURCE_DIRECTORY} && zip -r ../{LOCAL_LAMBDA_PACKAGE_FILENAME} ."
+    )
+    print("Module built")
+
+
+def upload() -> None:
+    print("Uploading ...")
+    with open(LOCAL_LAMBDA_PACKAGE_FILENAME, "rb") as lambda_package_file:
+        lambda_package = lambda_package_file.read()
+    lambda_client = boto3.client("lambda")
+    lambda_client.update_function_code(
+        FunctionName=LAMBDA_FUNCTION_NAME, ZipFile=lambda_package
+    )
+    print("Uploaded")
+
+
+def deploy() -> None:
+    build_module()
+    upload()
+
+
+if __name__ == "__main__":
+    deploy()
