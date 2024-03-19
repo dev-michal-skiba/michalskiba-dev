bb81d8baf3ef5a2e6412ed9096cf7e7ae8c2dd43
dev-michal-skiba <dev.michal.skiba@gmail.com>
2024-03-19 23:07:05 +0100

    Fixed deployment script

--- modules/sql_injection/bin/deploy.py
+++ modules/sql_injection/bin/deploy.py
@@ -2,7 +2,7 @@ import os
 
 import boto3
 
-LOCAL_SOURCE_DIRECTORY = "src/"
+LOCAL_SOURCE_DIRECTORY = ".aws-sam/build/SQLInjectionAPI"
 LOCAL_LAMBDA_PACKAGE_FILENAME = "lambda_package.zip"
 LAMBDA_FUNCTION_NAME = "michalskiba-dev-sql-injection"
 
@@ -11,8 +11,7 @@ def build_module() -> None:
     print("Building module ...")
     os.remove(LOCAL_LAMBDA_PACKAGE_FILENAME)
     os.system(
-        f"cd {LOCAL_SOURCE_DIRECTORY} && "
-        f"zip -r ../{LOCAL_LAMBDA_PACKAGE_FILENAME} . -x template.yaml"
+        f"cd {LOCAL_SOURCE_DIRECTORY} && " f"zip -r ../../../{LOCAL_LAMBDA_PACKAGE_FILENAME} ."
     )
     print("Module built")
 
@@ -22,9 +21,7 @@ def upload() -> None:
     with open(LOCAL_LAMBDA_PACKAGE_FILENAME, "rb") as lambda_package_file:
         lambda_package = lambda_package_file.read()
     lambda_client = boto3.client("lambda")
-    lambda_client.update_function_code(
-        FunctionName=LAMBDA_FUNCTION_NAME, ZipFile=lambda_package
-    )
+    lambda_client.update_function_code(FunctionName=LAMBDA_FUNCTION_NAME, ZipFile=lambda_package)
     print("Uploaded")
 
 
