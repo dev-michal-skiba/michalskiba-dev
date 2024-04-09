3faf25831771a2b26eb619f0ac67cc9a1589eb9f
dev-michal-skiba <dev.michal.skiba@gmail.com>
2024-04-09 18:38:12 +0200

    Add insecure parcel stores querying

--- modules/sql_injection/src/sql_injection/db.py
+++ modules/sql_injection/src/sql_injection/db.py
@@ -20,7 +20,27 @@ class ParcelStore(BaseModel):
 def get_parcel_stores(
     address_search_phrase: str, is_secure_version_on: bool
 ) -> list[dict[str, str]]:
+    if is_secure_version_on:
+        return get_parcel_stores_secure(address_search_phrase)
+    return get_parcel_stores_insecure(address_search_phrase)
+
+
+def get_parcel_stores_secure(address_search_phrase: str) -> list[dict[str, str]]:
     queryset = ParcelStore.select().where(ParcelStore.address.contains(address_search_phrase))
+    return [
+        {
+            "name": parcel_store.name,
+            "address": parcel_store.address,
+            "opening_hours": parcel_store.opening_hours,
+            "access_code": parcel_store.access_code,
+        }
+        for parcel_store in queryset
+    ]
+
+
+def get_parcel_stores_insecure(address_search_phrase: str) -> list[dict[str, str]]:
+    sql = f"SELECT * FROM parcelstore WHERE address LIKE '%{address_search_phrase}%'"
+    queryset = ParcelStore.raw(sql)
     return [
         {
             "name": parcel_store.name,
--- modules/sql_injection/tests/test_db.py
+++ modules/sql_injection/tests/test_db.py
@@ -5,16 +5,17 @@ from sql_injection.db import get_parcel_stores
 
 class TestGetParcelStores:
     @pytest.mark.parametrize(
-        "address_search_phrase, expected_parcel_stores",
+        "is_secure_version_on, expected_parcel_stores",
         (
+            (True, []),
             (
-                "",
+                False,
                 [
                     {
-                        "name": "parcel_store_1",
-                        "address": "Red Street 1, 00-001 Warsaw, Poland",
-                        "opening_hours": "8:00-18:00",
-                        "access_code": "743763",
+                        "name": "3.40.1",
+                        "address": None,
+                        "opening_hours": None,
+                        "access_code": None,
                     },
                     {
                         "name": "parcel_store_2",
@@ -22,19 +23,32 @@ class TestGetParcelStores:
                         "opening_hours": "7:00-15:00",
                         "access_code": "951620",
                     },
-                    {
-                        "name": "parcel_store_3",
-                        "address": "Green Street 3, 00-001 Warsaw, Poland",
-                        "opening_hours": "9:00-17:00",
-                        "access_code": "477584",
-                    },
                 ],
             ),
         ),
     )
-    def test_correct_parcel_stores_returned(
-        self, address_search_phrase: str, expected_parcel_stores: list[dict[str, str]]
+    def test_sql_injection(
+        self, is_secure_version_on: bool, expected_parcel_stores: list[dict[str, str]]
     ) -> None:
-        parcel_stores = get_parcel_stores(address_search_phrase, is_secure_version_on=True)
+        parcel_stores = get_parcel_stores(
+            address_search_phrase="Wroclaw%' UNION SELECT NULL, sqlite_version(), NULL, NULL, NULL;--",
+            is_secure_version_on=is_secure_version_on,
+        )
 
         assert parcel_stores == expected_parcel_stores
+
+    @pytest.mark.parametrize("is_secure_version_on", (True, False))
+    def test_normal_query(self, is_secure_version_on: bool) -> None:
+        parcel_stores = get_parcel_stores(
+            address_search_phrase="Wroclaw",
+            is_secure_version_on=is_secure_version_on,
+        )
+
+        assert parcel_stores == [
+            {
+                "name": "parcel_store_2",
+                "address": "Blue Street 2, 47-404 Wroclaw, Poland",
+                "opening_hours": "7:00-15:00",
+                "access_code": "951620",
+            }
+        ]
