b10cb8ab18ba955c4750a6a9d40673a35c08d87a
dev-michal-skiba <dev.michal.skiba@gmail.com>
2025-05-31 18:48:31 +0200

    Added prettier formater

--- modules/frontend/src/themes/michalskiba_dev/static/js/demo/host_header_injection/script.js
+++ modules/frontend/src/themes/michalskiba_dev/static/js/demo/host_header_injection/script.js
@@ -1,101 +1,111 @@
 function getValidatedEmail() {
-    const email = document.getElementById("email").value;
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (emailRegex.test(email)) {
-        return email;
-    }
-    return null;
+  const email = document.getElementById("email").value;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (emailRegex.test(email)) {
+    return email;
+  }
+  return null;
 }
 
 function clearError() {
-    document.getElementById("error-message").innerText = "";
+  document.getElementById("error-message").innerText = "";
 }
 
 function showError(message) {
-    document.getElementById("error-message").innerText = message;
+  document.getElementById("error-message").innerText = message;
 }
 
 function updateModalAndShow(email, resetLink) {
-    const modalEmail = document.getElementById('email-email');
-    modalEmail.innerText = email;
+  const modalEmail = document.getElementById("email-email");
+  modalEmail.innerText = email;
 
-    const modalLink = document.getElementById('reset-link');
-    modalLink.href = resetLink;
+  const modalLink = document.getElementById("reset-link");
+  modalLink.href = resetLink;
 
-    const emailModal = new bootstrap.Modal(document.getElementById('email-modal'));
-    emailModal.show();
+  const emailModal = new bootstrap.Modal(
+    document.getElementById("email-modal"),
+  );
+  emailModal.show();
 }
 
 function submitForm() {
-    clearError();
-    const email = getValidatedEmail();
-    if (email === null) {
-        showError("Please enter a valid email address");
-        return;
-    }
-    window.callApi({
-        method: "POST",
-        path: "/demo/host-header-injection/password-reset/initiate",
-        body: { email },
-    }).then(
-        response => response.json()
-    ).then(
-        data => {
-            updateModalAndShow(email, data.reset_link);
-        }
-    );
-}
-
-function submitNewPassword() {
-    clearError();
-    const token = new URLSearchParams(window.location.search).get('token');
-    const password = document.getElementById("new-password").value;
-    if (!password) {
-        showError("Please enter a new password");
-        return;
-    }
-    window.callApi({
-        method: "POST",
-        path: "/demo/host-header-injection/password-reset/complete",
-        body: { token, password },
+  clearError();
+  const email = getValidatedEmail();
+  if (email === null) {
+    showError("Please enter a valid email address");
+    return;
+  }
+  window
+    .callApi({
+      method: "POST",
+      path: "/demo/host-header-injection/password-reset/initiate",
+      body: { email },
     })
-    .then(response => {
-        if (response.status !== 204) {
-            throw new Error("Failed to reset password, please try again.");
-        }
-        window.location.href = "/demo/host-header-injection/password-reset/success";
-    })
-    .catch(error => {
-        showError(error.message);
+    .then((response) => response.json())
+    .then((data) => {
+      updateModalAndShow(email, data.reset_link);
     });
 }
 
-window.onload = function() {
-    document.getElementById("version-switch").checked = window.getIsSecureVersionOn();
+function submitNewPassword() {
+  clearError();
+  const token = new URLSearchParams(window.location.search).get("token");
+  const password = document.getElementById("new-password").value;
+  if (!password) {
+    showError("Please enter a new password");
+    return;
+  }
+  window
+    .callApi({
+      method: "POST",
+      path: "/demo/host-header-injection/password-reset/complete",
+      body: { token, password },
+    })
+    .then((response) => {
+      if (response.status !== 204) {
+        throw new Error("Failed to reset password, please try again.");
+      }
+      window.location.href =
+        "/demo/host-header-injection/password-reset/success";
+    })
+    .catch((error) => {
+      showError(error.message);
+    });
+}
 
-    const passwordResetInitiateForm = document.getElementById("password-reset-initiate-form");
-    if (passwordResetInitiateForm) {
-        passwordResetInitiateForm.addEventListener("submit", (event) => {
-            event.preventDefault();
-            submitForm();
-        });
-    }
+window.onload = function () {
+  document.getElementById("version-switch").checked =
+    window.getIsSecureVersionOn();
 
-    const submitPasswordResetInitiateButton = document.getElementById("submit-password-reset-initiate-button");
-    if (submitPasswordResetInitiateButton) {
-        submitPasswordResetInitiateButton.addEventListener("click", submitForm);
-    }
+  const passwordResetInitiateForm = document.getElementById(
+    "password-reset-initiate-form",
+  );
+  if (passwordResetInitiateForm) {
+    passwordResetInitiateForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      submitForm();
+    });
+  }
 
-    const passwordResetForm = document.getElementById("password-reset-form");
-    if (passwordResetForm) {
-        passwordResetForm.addEventListener("submit", (event) => {
-            event.preventDefault();
-            submitForm();
-        });
-    }
+  const submitPasswordResetInitiateButton = document.getElementById(
+    "submit-password-reset-initiate-button",
+  );
+  if (submitPasswordResetInitiateButton) {
+    submitPasswordResetInitiateButton.addEventListener("click", submitForm);
+  }
 
-    const submitPasswordResetButton = document.getElementById("submit-password-reset-button");
-    if (submitPasswordResetButton) {
-        submitPasswordResetButton.addEventListener("click", submitNewPassword);
-    }
+  const passwordResetForm = document.getElementById("password-reset-form");
+  if (passwordResetForm) {
+    passwordResetForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      submitForm();
+    });
+  }
+
+  const submitPasswordResetButton = document.getElementById(
+    "submit-password-reset-button",
+  );
+  if (submitPasswordResetButton) {
+    submitPasswordResetButton.addEventListener("click", submitNewPassword);
+  }
 };
--- modules/frontend/src/themes/michalskiba_dev/static/js/demo/script.js
+++ modules/frontend/src/themes/michalskiba_dev/static/js/demo/script.js
@@ -1,58 +1,60 @@
 function setIsSecureVersionOn(value) {
-    localStorage.setItem("is_secure_version_on", value);
+  localStorage.setItem("is_secure_version_on", value);
 }
 
 function getIsSecureVersionOn() {
-    const is_secure_version_on = localStorage.getItem("is_secure_version_on");
-    if (is_secure_version_on == null) {
-        setIsSecureVersionOn("true");
-        return true;
-    }
-    return is_secure_version_on.toLowerCase() !== "false";
+  const is_secure_version_on = localStorage.getItem("is_secure_version_on");
+  if (is_secure_version_on == null) {
+    setIsSecureVersionOn("true");
+    return true;
+  }
+  return is_secure_version_on.toLowerCase() !== "false";
 }
 
 function versionSwitchClick() {
-    let is_secure_version_on = getIsSecureVersionOn();
-    setIsSecureVersionOn(!is_secure_version_on);
-    window.location.reload();
+  let is_secure_version_on = getIsSecureVersionOn();
+  setIsSecureVersionOn(!is_secure_version_on);
+  window.location.reload();
 }
 
 function customEncodeURI(value) {
-    return encodeURIComponent(value).replace(/-/g, "%2D");
+  return encodeURIComponent(value).replace(/-/g, "%2D");
 }
 
 function getQueryParamsString(queryParams) {
-    queryParams['is_secure_version_on'] = getIsSecureVersionOn();
-    const queryParamsString = Object.entries(queryParams).map(([key, value]) => `${key}=${customEncodeURI(value)}`).join('&');
-    if (queryParamsString.length > 0) {
-        return '?' + queryParamsString;
-    }
-    return '';
+  queryParams["is_secure_version_on"] = getIsSecureVersionOn();
+  const queryParamsString = Object.entries(queryParams)
+    .map(([key, value]) => `${key}=${customEncodeURI(value)}`)
+    .join("&");
+  if (queryParamsString.length > 0) {
+    return "?" + queryParamsString;
+  }
+  return "";
 }
 
 async function callApi({
-    method,
-    path,
-    queryParams = {},
-    body = null,
-    credentials = "same-origin",
+  method,
+  path,
+  queryParams = {},
+  body = null,
+  credentials = "same-origin",
 } = {}) {
-    if (body != null) {
-        body = JSON.stringify(body);
-    }
-    const options = {
-        method: method,
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body,
-        credentials,
-    };
+  if (body != null) {
+    body = JSON.stringify(body);
+  }
+  const options = {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body,
+    credentials,
+  };
 
-    return await fetch(
-        window.HUGO_API_URL + path + getQueryParamsString(queryParams),
-        options,
-    );
+  return await fetch(
+    window.HUGO_API_URL + path + getQueryParamsString(queryParams),
+    options,
+  );
 }
 
 window.versionSwitchClick = versionSwitchClick;
